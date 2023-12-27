a496d97a0a6f9b5496bcc34a7249bc0cac2b0a08
kpapap <konstantinosp@renvis.gr>
2023-12-27 15:12:49 +0200

    edited native crs

--- datacube_ows/ows_cfg_elbios1.py
+++ datacube_ows/ows_cfg_elbios1.py
@@ -623,7 +623,7 @@ ows_cfg = {
                     "resource_limits": standard_resource_limits,
                     # Near Real Time datasets are being regularly updated - do not cache ranges in memory.
                     "dynamic": True,
-                    "native_crs": "EPSG:3577",
+                    "native_crs": "EPSG:3857",
                     "native_resolution": [10.0, 10.0],
                     # "flags": {
                     #     "band": "mask25",
